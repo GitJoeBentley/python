e61e0c9a74d656f8c241d4467d24ced48308e920
GitJoeBentley <88294868+GitJoeBentley@users.noreply.github.com>
2023-08-01 17:07:31 -0700

    Update cis29_ex14-2.py

--- review/HashTables/cis29_ex14-2.py
+++ review/HashTables/cis29_ex14-2.py
@@ -7,29 +7,30 @@ http://voyager.deanza.edu/~bentley/cis29/examples/ex14-2.cpp
 
 __author__ = "Joe Bentley, joe.deanza@gmail.com"
 
+number_of_buckets = 100000
+
 class Dictionary:
     def __init__(self, filename: str):
-        self._number_of_buckets = 100000
-        self._dictionary = [None for i in range(self._number_of_buckets)]
+        self._dictionary = [None for i in range(number_of_buckets)]
         temp = (open(filename).read().split('\n'))
         self._number_of_words = len(temp)
         self._number_of_words_not_stored = 0
         self._number_of_buckets_used = 0
         for word in temp:
-            index = hash(temp)
-            if not self._dictionary[index]:
-                self._dictionary[index] = temp
+            index = hash(word)
+            if index and not self._dictionary[index]:
+                self._dictionary[index] = word
                 self._number_of_buckets_used += 1
             else:
                 self._number_of_words_not_stored += 1
         print("number of buckets used = ", self._number_of_buckets_used)
         print("number of words not stored =" , self._number_of_words_not_stored)
         print("number of words = ", self._number_of_words),
-            
-        
+
     def find_scambled_word(self, scramble_word: str) -> str:
         index = hash(scramble_word)
-        return self._dictionary[index]
+        word = self._dictionary[index]
+        return word
         
 
 def hash(text: str) -> int:
@@ -39,12 +40,22 @@ def hash(text: str) -> int:
     """
 
     primes =(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73,
-                  79, 83, 89, 97, 101);
+                  79, 83, 89, 97, 101)
     product = 1;
     for letter in text:
-        product *= primes[ord(letter.lower())  - ord('a')]
+        index = ord(letter.lower())  - ord('a')
+        if index < 0 or index >= len(primes):
+            return None
+        product *= primes[index]
     return product % number_of_buckets
+
 
 if __name__ == '__main__':
     dictionary = Dictionary('words')
+    while (True):
+        scrambled_word = input('Enter a scrambled word (\"quit\" to exit) => ')
+        if scrambled_word == 'quit':
+            exit(0)
+        else:
+            print('unscramble =',dictionary.find_scambled_word(scrambled_word))                              
                             
